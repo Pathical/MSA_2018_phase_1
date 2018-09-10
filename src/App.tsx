0f9cb55cf7b5a71ba6c0b45f50d9429149a99451
Jack <pathicalmine@gmail.com>
2018-09-10 18:28:35 +1200

    can now get 7 days forecast. Displays an image when there is an error. Improved GUI a bit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 
 import * as React from 'react';
-// import './App.css';
 import WeatherForADay from './components/WeatherForADay';
-// import './css/styles.css'
-
 
 interface IState {
   error: any,
-  result: any
+  result: any,
+  days: any
 }
-
 
 export default class App extends React.Component<{}, IState> {
 
   constructor(props: any) {
     super(props);
-    this.state = {error:'', result: '' }
+    this.state = { error: '', result: '', days: 7 }
     this.handleOnChange = this.handleOnChange.bind(this);
   }
-
 
   public handleOnChange(event: any) {
     if (event.key === 'Enter') {
@@ -26,15 +22,13 @@ export default class App extends React.Component<{}, IState> {
     }
   }
 
-
   public getWeatherbyCityName(cityName: any) {
-    fetch('https://api.weatherbit.io/v2.0/forecast/daily?key=e7739c749efd4516afe5091f2010478b&days=1&city=' + cityName, {
+    fetch('https://api.weatherbit.io/v2.0/forecast/daily?key=e7739c749efd4516afe5091f2010478b&days=' + this.state.days + '&city=' + cityName, {
       method: 'GET'
     }).then((response: any) => {
-
-      if (response.status !== 200){
+      if (response.status !== 200) {
         this.setState({
-          error: 'An error occured, please check your spelling, or try again later.',
+          error: response.status,
           result: ''
         });
       } else {
@@ -44,26 +38,58 @@ export default class App extends React.Component<{}, IState> {
             result: responseBody
           });
         })
-      } 
+      }
     });
   }
 
+  public createCard = () => {
+    const cards = []
+    for (let i = 0; i < this.state.days; i++) {
+      cards.push(this.state.result === '' ? '' : <WeatherForADay dataForThisDay={this.state.result.data[i]} cityName={this.state.result.city_name} countryCode={this.state.result.country_code} />)
+    }
+    return cards
+  }
 
+  // This method gets the image which represents the weather type 
+  public getIcon(): string {
+    return "https://http.cat/" + this.state.error
+  }
 
   public render() {
     return (
       <div >
-        <div className='dank'> 
-          Please enter a city or country 
+        <div className='dank'>
+          <text>Please enter a city or country<br /></text>
           <input
             onKeyPress={this.handleOnChange}
           />
         </div>
-        {this.state.result === '' ? '' : <WeatherForADay dataForThisDay={this.state.result.data[0]} cityName={this.state.result.city_name} countryCode={this.state.result.country_code}/>}
-        {this.state.error === '' ? '' : this.state.error}
-      </div>
 
+        <div>
+          {this.createCard()}
+        </div>
+
+        <div className='dank'>
+          {this.state.error === '' ? '' : 'An error occured, please check your spelling, or try again later.'}
+        </div>
+
+        <div className='dank'>
+          {this.state.error === '' ? '' : <img src={this.getIcon()} width='50%' />}
+        </div>
+
+        <footer className="text-center bottom-space">
+          <p>
+            Developed by
+            <a href="https://github.com/Pathical"> @Jack Mao</a>
+            .
+            Special thanks to
+            <a href="https://www.weatherbit.io/"> @weatherbit</a>
+            , and
+            <a href="https://http.cat/"> @Cat Images</a>
+            .
+          </p>
+        </footer>
+      </div>
     );
   }
-
 }
--- src/components/WeatherForADay.tsx
+++ src/components/WeatherForADay.tsx
@@ -2,50 +2,63 @@
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-// import CardMedia from '@material-ui/core/CardMedia';
 import * as React from 'react';
-
-
 
 
 
 class WeatherForADayApp extends React.Component<{ dataForThisDay: any, cityName: any, countryCode: any }> {
 
+    // This method gets the image which represents the weather type 
     public getIcon(): string {
         return "https://www.weatherbit.io/static/img/icons/" + this.props.dataForThisDay.weather.icon + ".png"
     }
 
+    // This method takes a string, converts it to a number, rounds it.
+    public roundString(arg: string): number{
+        return Math.round(Number.parseFloat(arg));
+    }
+
+    // Takes a string representation of a date (from the RESTful api)
+    // Creates a new Date object
+    // Take only the necessary parts of the Date object
+    public convertDate(arg: string): string{
+        const converted = new Date(arg);        const splitted = converted.toDateString().split(' ');
+        return splitted[0] + ' ' + splitted[2] + ' ' + splitted[1] + ' ' + splitted[3];
+    }
+
+    // This render function helps create a Card from material UI
+    // Shows weather information for a particular date
     public render() {
         return (
-            <div className="weatherCard">
-                <Card>
-                    <CardContent >
-                        <Typography gutterBottom={false} variant="headline" component="h2" style={{ fontSize: 50}}>
-                            {this.props.cityName},{this.props.countryCode}
+            <div className='col-xs-1'>
+                <Card style={{borderRadius:"20px"}} className='MuiPaper-elevation1-12 weatherCard'>
+                    <CardContent>
+                        <Typography gutterBottom={false} style={{ fontSize: "2.5em"}}>
+                            {this.props.cityName}, {this.props.countryCode}
                         </Typography>
-                        <Typography component="p" style={{ fontSize: 25}}  >
+                        <Typography style={{ fontSize: "1.6em"}}  >
                             {this.props.dataForThisDay.weather.description}
                         </Typography>
                         <div >
-                            <img src={this.getIcon()} />
+                            <img src={this.getIcon()} width='50%' />
                         </div>
-                        <Typography component="p" style={{ fontSize: 35 }}>
+                        <Typography  style={{ fontSize: "2em" }}>
                             {this.props.dataForThisDay.max_temp} °C
                         </Typography>
-                        <Typography component="p" style={{ fontSize: 20 }}>
+                        <Typography  style={{ fontSize: "1.6em" }}>
+                            {this.convertDate(this.props.dataForThisDay.valid_date)}
+                        </Typography>
+                        <Typography  style={{ fontSize: "1.2em" }}>
                             Percipitation: {this.props.dataForThisDay.pop} %
                         </Typography>
-                        <Typography component="p" style={{ fontSize: 20 }}>
+                        <Typography  style={{ fontSize: "1.2em" }}>
                             Humidity: {this.props.dataForThisDay.rh} %
                         </Typography>
-                        <Typography component="p" style={{ fontSize: 20 }}>
-                            Wind speed: {this.props.dataForThisDay.wind_gust_spd} km/h
+                        <Typography  style={{ fontSize: "1.2em" }}>
+                            Wind speed: {this.roundString(this.props.dataForThisDay.wind_gust_spd)} km/h
                         </Typography>
-                        <Typography component="p" style={{ fontSize: 20 }}>
-                            UV index: {this.props.dataForThisDay.uv}
-                        </Typography>
-                        <Typography component="p" style={{ fontSize: 20 }}>
-                            Date: {this.props.dataForThisDay.valid_date}
+                        <Typography  style={{ fontSize: "1.2em" }}>
+                            UV index: {this.roundString(this.props.dataForThisDay.uv)}
                         </Typography>
                     </CardContent>
                 </Card>
